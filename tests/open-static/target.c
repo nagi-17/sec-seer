78dab61386757fa166976d394be20872d2124253
ansh <anshdav0@gmail.com>
2026-08-09 21:08:41 +0530

    feat: testcase which give two seperate syscall error randomly

--- tests/open-static/target.c
+++ tests/open-static/target.c
@@ -1,13 +1,47 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
 
-int main(void) {
-    printf("before open()\n");
-    fflush(stdout);
+int main(int argc, char *argv[]) {
 
-    int fd = open("/etc/hostname", O_RDONLY);  // TRACER_TEST_MARKER_LINE
+    srand(time(NULL)); 
+    int raw_random = rand();
 
-    printf("after open(), fd=%d\n", fd);
-    fflush(stdout);
-    return 0;
+        if(raw_random % 2){
+
+        const char *path = (argc > 1) ? argv[1] : "/etc/hostname";
+
+        int fd = open(path, O_RDONLY);
+
+        printf("before read()\n");
+        fflush(stdout);
+
+        char buf[64];
+        ssize_t n = read(fd, buf, sizeof(buf));  // TRACER_TEST_MARKER_LINE
+
+        printf("after read(), n=%zd\n", n);
+        fflush(stdout);
+        return 0;
+    }
+    else {
+        int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+        struct sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(1);
+        inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+
+        printf("before connect()\n");
+        fflush(stdout);
+
+        int rc = connect(sock, (struct sockaddr *)&addr, sizeof(addr));  // TRACER_TEST_MARKER_LINE
+
+        printf("after connect(), rc=%d\n", rc);
+        fflush(stdout);
+        return 0;
+    }
 }
